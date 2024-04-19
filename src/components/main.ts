be5857cde91d68475ccf8250eda5f8793997c93b
matenagy <mate.nagy@feat.hu>
2024-04-19 08:53:07 +0200

    cleeanup

--- src/components/main.ts
+++ src/components/main.ts
@@ -1,28 +1,16 @@
-import { App } from 'vue';
 import { FormifyForm, FormifyInput, FormifyCheckbox, FormifyRadio, FormifyError } from './VueFormify';
 import { FormType, ComponentProps } from './index';
 import { createInput } from '@/composable/createInput';
 import VueFormify from '@/plugin/plugin';
 
-
-export default {
-	install: (app: App) => {
-		app.component('FormifyForm', FormifyForm);
-		app.component('FormifyInput', FormifyInput);
-		app.component('FormifyCheckbox', FormifyCheckbox);
-		app.component('FormifyRadio', FormifyRadio);
-		app.component('FormifyError', FormifyError);
-	},
-};
-
 export {
 	createInput,
 	VueFormify,
 	FormifyForm,
+	FormifyError,
 	FormifyInput,
 	FormifyCheckbox,
 	FormifyRadio,
-	FormifyError,
 };
 
 export type {
