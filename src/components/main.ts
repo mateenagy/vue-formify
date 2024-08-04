ab46432eac86004c93ed5509b9de056f3b52994a
matenagy <mate.nagy@feat.hu>
2024-08-04 19:34:54 +0200

    add: createSchema export

--- src/components/main.ts
+++ src/components/main.ts
@@ -4,12 +4,14 @@ import { default as FormifyForm } from './FormElements/Form.vue';
 import { default as FormifyError } from './FormElements/Error.vue';
 import { FormType, ComponentProps } from './index';
 import { createInput } from '@/composable/createInput';
+import { createSchema } from '@/composable/createSchema';
 
 const ArrayField = createInput<ComponentProps<typeof ArrayComponent>>(ArrayComponent, { default: [] });
 const Field = createInput<ComponentProps<typeof FieldComponent>>(FieldComponent);
 
 export {
 	createInput,
+	createSchema,
 	FormifyForm,
 	FormifyError,
 	ArrayField,
