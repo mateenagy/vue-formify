2cf7bdc759c6630805e16c2a48466a0a753b3521
matenagy <mate.nagy@feat.hu>
2025-05-29 18:29:14 +0200

    remove unocss-preset-grid

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,6 @@ import presetUno from '@unocss/preset-uno';
 import transformerVariantGroup from '@unocss/transformer-variant-group';
 import transformerDirectives from '@unocss/transformer-directives'
 import { defineConfig } from 'unocss';
-import { presetGrid } from 'unocss-preset-grid';
 
 const DIRECTION = {
 	t: 'top',
@@ -14,10 +13,6 @@ const DIRECTION = {
 export default defineConfig({ 
 	presets: [
 		presetUno(),
-		presetGrid({
-			gutter: '24px',
-			piece: '0px'
-		}),
 	],
 	transformers: [
 		transformerVariantGroup(),
